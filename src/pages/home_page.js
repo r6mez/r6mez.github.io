09fa533d73f1976d717aa60adf43c1dcd024a28b
Ramez <iramezdev@gmail.com>
2022-02-11 19:10:25 +0200

    home page

--- src/pages/home_page.js
+++ src/pages/home_page.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import planet from "../imgs/planet.png";
 
 class HomePage extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Home Page</h1>
+            <div className='home-page'>
+                <div className='home-text'>
+                    <p className='name-text'>Ramez</p>
+                    <p className='name-text'>Medhat</p>
+                    <p className='skill-text'>- Programmer</p>
+                </div>
+                <div className='img-container'>
+                    <img src={planet} className='planet-img'></img>
+                </div>
             </div>
         );
     }
