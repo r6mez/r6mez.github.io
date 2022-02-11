da6e4b2e2d77d09fca6005406c1b4d3776302d00
Ramez <iramezdev@gmail.com>
2022-02-11 10:48:08 +0200

    adding the font

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import './fonts/WorkSans-Bold.ttf'
+import './fonts/WorkSans-SemiBold.ttf'
+import './fonts/WorkSans-Medium.ttf'
+
 
 ReactDOM.render(
   <React.StrictMode>
